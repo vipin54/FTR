5300f040f42e623666ca8f0929b7c31818f0785e
nitin <yadav.nitin0104@gmail.com>
2024-07-27 17:45:42 +0530

    First Commit

---
+++ FTRAPP/src/test/java/com/org/ftr/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.ftr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
